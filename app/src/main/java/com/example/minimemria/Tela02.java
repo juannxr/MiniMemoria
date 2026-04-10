606cb3d87b2ff523c1cb0e44e7f59736a8813bde
aluno <hiro.nakamura@alunos.ifsuldeminas.edu.br>
2026-04-10 11:26:15 -0300

    Versão 1.3

--- app/src/main/java/com/example/minimemria/Tela02.java
+++ app/src/main/java/com/example/minimemria/Tela02.java
@@ -1,10 +1,13 @@
 package com.example.minimemria;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.Touch;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class Tela02 extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn;
-    private EditText textNome;
+    private EditText textNome,textIdade;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,18 +34,23 @@ public class Tela02 extends AppCompatActivity implements View.OnClickListener {
         btn = findViewById(R.id.button2);
         btn.setOnClickListener(this);
         textNome = findViewById(R.id.editTextText5);
+        textIdade = findViewById(R.id.editTextText6);
     }
 
+    @SuppressLint("NewApi")
     @Override
     public void onClick(View v) {
         if(v == btn){
+   if(!textNome.getText().isEmpty() && !textIdade.getText().isEmpty()){
+    Intent i = new Intent(this, Tela03.class);
+    Bundle caixa = new Bundle();
+    caixa.putString("nome", textNome.getText().toString());
+    i.putExtras(caixa);
+    startActivity(i);
+}else{
+       Toast.makeText(this,"*Campo obrigatório",Toast.LENGTH_SHORT).show();
 
-            Intent i = new Intent(this, Tela03.class);
-            Bundle caixa = new Bundle();
-            caixa.putString("nome", textNome.getText().toString());
-            i.putExtras(caixa);
-            startActivity(i);
-
+   }
         }
     }
 }
